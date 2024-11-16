7d24e17440e3eb5e747e0f8bdef6e82c2dd1b531
lilyqin7 <lilyqin527@gmail.com>
2024-11-15 19:01:49 -0500

    Update Lily.py

--- Lily.py
+++ Lily.py
@@ -1,10 +1,22 @@
 from cmu_graphics import *
 
-# def onAppStart(app):
+def onAppStart(app):
+    app.width = 800
+    app.height = 500
+    app.startWidth = 400
+    app.startHeight = 400
 
-# def redrawAll(app):
+#change the really ugly font
+def redrawAll(app):
+    drawRect(200, 50, app.startWidth, app.startHeight, fill = 'lightGreen')
+    drawLabel('Replace with game name', app.width/2, 80, size = 30)
+    drawRect(360, 320, 80, 25, fill = 'lightPink')
+    drawLabel('Instructions!', app.width/2, 330)
+    drawRect(375, 350, 50, 25, fill = 'lightPink')
+    drawLabel('Start!', app.width/2, 360)
+
 
 def main():
-    runApp()
+    runApp(width = app.width, height = app.height)
 
 main()
