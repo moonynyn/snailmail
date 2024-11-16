39eca9e44cd4aa37002916db30969c0ab36c052f
lilyqin7 <lilyqin527@gmail.com>
2024-11-15 19:55:33 -0500

    Update Lily.py

--- Lily.py
+++ Lily.py
@@ -5,16 +5,28 @@ def onAppStart(app):
     app.height = 500
     app.startWidth = 400
     app.startHeight = 400
+    app.instructionsTop = 360
+    app.instructionsLeft = 320
+    app.instructionsWidth = 80
+    app.instructionsHeight = 25
 
 #change the really ugly font
 def redrawAll(app):
     drawRect(200, 50, app.startWidth, app.startHeight, fill = 'lightGreen')
     drawLabel('Replace with game name', app.width/2, 80, size = 30)
-    drawRect(360, 320, 80, 25, fill = 'lightPink')
+    drawRect(app.instructionsTop, app.instructionsLeft, app.instructionsWidth, app.instructionsHeight, fill = 'lightPink')
     drawLabel('Instructions!', app.width/2, 330)
     drawRect(375, 350, 50, 25, fill = 'lightPink')
     drawLabel('Start!', app.width/2, 360)
 
+#if instructions button is pressed
+def onMousePress(app, mouseX, mouseY):
+    if intersection(app, mouseX, mouseY):
+        drawRect()
+        
+def intersection(app, mouseX, mouseY):
+    if (mouseX >= app.instructionsLeft and mouseX <= app.instructionsLeft + app.instructionsWidth) and (mouseY <= app.instructionsTop and mouseY >= app.instructionsHeight + app.instructionsTop):
+        print('hi')
 
 def main():
     runApp(width = app.width, height = app.height)
