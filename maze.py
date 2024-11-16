16d32d819c6d886bf2ba82ea8fd0a91e2efa5556
lilyqin7 <lilyqin527@gmail.com>
2024-11-15 20:47:13 -0500

    Update maze.py

--- maze.py
+++ maze.py
@@ -1,9 +1,26 @@
 from cmu_graphics import *
 
 def onAppStart(app):
+    app.width = 600
+    app.height = 600
+    app.blocksWide = 30
+    app.blocksTall = 30
+
+def drawBackground(app):
+    drawRect(0, 0, app.width, app.height, fill = 'forestGreen')
 
 def redrawAll(app):
-    drawRect()
+    drawBackground(app)
+    #draws the grid for the maze
+    numBlocksTall = app.height/app.blocksTall
+    numBlocksWide = app.width/app.blocksWide
+    for x in range(int(numBlocksWide)):
+        x *= app.blocksWide
+        drawLine(x, 0, x, app.height, fill = 'darkGreen')
+    for y in range(int(numBlocksTall)):
+        y *= app.blocksTall
+        drawLine(0, y, app.width, y, fill = 'darkGreen')
+
 
 def main():
     runApp()
